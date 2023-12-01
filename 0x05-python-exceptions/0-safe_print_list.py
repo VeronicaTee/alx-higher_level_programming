18734ae694fff1bee68082f7942685fb148e173a
VeronicaTee <veronicatomilola@gmail.com>
2023-12-01 14:05:43 -0800

    apply pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,14 +3,13 @@
 def safe_print_list(list, x):
     count = 0
     res = []
-    for i in list[ : x]:
+    for i in list[: x]:
         try:
             res.append(i)
         except TypeError:
             print('Cannot perform operation on different types', arg)
         else:
-            count+=1
-    
+            count += 1
 
-    print(*res, sep = "")
+    print(*res, sep="")
     return count
